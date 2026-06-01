3cc51b4c8a6004f7b75518f4944613fd217f4bcf
RickyBytes <76111028+Rith-byte@users.noreply.github.com>
2026-06-01 16:39:17 -0400

    Update Tutor.java

--- Tutor.java
+++ Tutor.java
@@ -9,48 +9,33 @@ public class Tutor {
     private String uname;
     private int age;
     private ArrayList<String> tSubjects; 
-    private double rating;             
+    private double rating;            // Rithvik - Range: [1,5] 
     private int experienceYears;        
     
     public static ArrayList<String> tUnames = new ArrayList<>();
     public static ArrayList<Tutor> tutors = new ArrayList<>();
 
     
-public String getFname() { 
+    public String getFname() { 
         return fname; 
     }
-    public String getLname() { 
-        return lname; 
-    }
-    public int getAge() { 
-        return age; 
-    }
-    public ArrayList<String> getSubjects() { 
-        return subjects; 
-    }
-
-
-
-
-
-public void setFname(String fname) { 
-        this.fname = fname; 
-    }
-    public void setLname(String lname) { 
-        this.lname = lname; 
-    }
-    public void setAge(int age) { 
-        this.age = age; 
-    }
-    public void setSubjects(ArrayList<String> subjects) {
-        this.subjects = subjects; 
-    }
-
-
+    
+    public String getLname() {return lname;}
+    
+    public int getAge() {return age;}
+    
+    // Rithvik Bottom 3 ↓
+    public int getExperienceYears(){return experienceYears;}
+    public ArrayList<String> getSubjects() {return tSubjects;}
+    public double getRating(){return this.rating;}
+    
+    
+    public void setFname(String fname) {this.fname = fname;}
+    
+    public void setLname(String lname) {this.lname = lname;}
+    
+    public void setAge(int age) {this.age = age;}
+    
+    public void setSubjects(ArrayList<String> subjects) {this.tSubjects = subjects;}
 
 }
-
-
-
-
-
