dd187e5b909bee665d4683b12631ce61fa20ec69
RickyBytes <76111028+Rith-byte@users.noreply.github.com>
2026-06-10 10:53:07 -0400

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,10 +1,44 @@
-public class Admin{
+// Rithvik
+public class Admin {
+    private String username;
+    private String password;
+    private boolean signedIn;
 
-public void verifyTutor(Tutor t){
-t.setVerify(true);
-}
-public void unverifyTutor(Tutor t){
-t.setVerify(false);
-}
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.signedIn = false;
+    }
 
+    public boolean authenticate(String username, String password) {
+        if (this.username.equals(username) && this.password.equals(password)) {
+            this.signedIn = true;
+            return true;
+        }
+        return false;
+    }
+
+    public void signOut() {
+        this.signedIn = false;
+    }
+
+    public boolean isSignedIn() {
+        return signedIn;
+    }
+
+    public boolean verifyTutor(Tutor t) {
+        if (!signedIn) {
+            return false;
+        }
+        t.setVerify(true);
+        return true;
+    }
+
+    public boolean unverifyTutor(Tutor t) {
+        if (!signedIn) {
+            return false;
+        }
+        t.setVerify(false);
+        return true;
+    }
 }
